499aa035c97636d60bf40254da321dec51b1ca09
Keiko Oda <keiko713@gmail.com>
2014-06-21 05:02:24 +0900

    Working okay version

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,64 +1,31 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: [:show, :edit, :update, :destroy]
+  before_action :set_todo, only: [:finish, :destroy]
 
   # GET /todos
-  # GET /todos.json
   def index
     @todos = Todo.all
-  end
-
-  # GET /todos/1
-  # GET /todos/1.json
-  def show
-  end
-
-  # GET /todos/new
-  def new
     @todo = Todo.new
   end
 
-  # GET /todos/1/edit
-  def edit
+  # GET /todos/1
+  def finish
+    @todo.update(finished: true)
+    redirect_to :todos
   end
 
   # POST /todos
-  # POST /todos.json
   def create
     @todo = Todo.new(todo_params)
+    @todo.finished = false
 
-    respond_to do |format|
-      if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
-        format.json { render :show, status: :created, location: @todo }
-      else
-        format.html { render :new }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /todos/1
-  # PATCH/PUT /todos/1.json
-  def update
-    respond_to do |format|
-      if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
-        format.json { render :show, status: :ok, location: @todo }
-      else
-        format.html { render :edit }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
+    @todo.save
+    redirect_to :todos
   end
 
   # DELETE /todos/1
-  # DELETE /todos/1.json
   def destroy
     @todo.destroy
-    respond_to do |format|
-      format.html { redirect_to todos_url, notice: 'Todo was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to :todos
   end
 
   private
